1e583407ce8b09b874ddd043b4ee96f4ab4e03c3
aryannarwal1277-art <aryannarwal1277@gmail.com>
2025-08-03 17:20:37 +0530

    Add files via upload

---
+++ Assinment 1.py
@@ -0,0 +1,25 @@
+
+# Task 1 :
+'''
+a=input('Enter the first number :')
+a=float(a)
+b=input('Enter the second number : ')
+b=float(b)
+ad= a+b
+s= a-b
+m= a*b
+d= a/b
+print('Addition',ad)
+print('Subtraction',s)
+print('Multiplication',m)
+print('Division',d)
+'''
+
+# Task 2 :
+'''
+a=input('Enter your first name : ')
+a=str(a)
+b=input('Enter your last name : ')
+b=str(b)
+print('hello',a, b,'welcome to python world')
+'''
