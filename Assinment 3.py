576c8344388e2fca14362c2d4d844818a5449827
aryannarwal1277-art <aryannarwal1277@gmail.com>
2025-08-09 16:15:50 +0530

    Add files via upload

---
+++ Assinment 3.py
@@ -0,0 +1,32 @@
+# Task 1 :
+
+'''
+def factorial(n):
+    if n < 2:
+        return 1
+    else:
+        return n * factorial(n - 1)
+print('Enter the value of factorial : 5')
+# Now outside the function, we call it and print:
+result = print('Factorial of 5 is :',factorial(5))
+print(result)
+'''
+
+# Task 2 :
+
+'''
+def main():
+    num = float(input("Please enter a number: "))
+
+    sqrt_val = math.sqrt(num)
+    ln_val = math.log(num)
+    sin_val = math.sin(num)
+
+
+    print(f" Square root  : {sqrt_val}")
+    print(f"  logarithm : {ln_val}")
+    print(f" Sine : {sin_val}")
+
+if __name__ == "__main__":
+    main()
+'''
